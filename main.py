2543d5dccc26684090c31564588ab1280173336a
Petr-By <havbin92@gmail.com>
2017-06-06 14:24:31 +0200

    Update main.py
    faster drawing

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import numpy as np
 from keras.models import load_model
 from keras.models import Model
 import keras
-
+import math
 
 import random
 #k=np.load('data.npy')
@@ -69,16 +69,46 @@ class App(QMainWindow):
 
     def layerbuttonClicked(self,sample_index):
         sender = self.sender()
-        m = PlotCanvas(self, width=14, height=8,layer=sender.text(),sample_index=self.sample_index)
+        m = PlotCanvas(self, width=9, height=9,layer=sender.text(),sample_index=self.sample_index)
         print(self.sample_index)
         m.move(0,0)
         m.show()
         print(sender.text())
+        im=plotimage(self, width=4, height=4,sample_index=self.sample_index)
+        im.move(900,0)
+        im.show()
+
+class plotimage(FigureCanvas):
+    def __init__(self, parent=None, width=9, height=9, dpi=100,sample_index=None):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+
+        self.axes = fig.add_subplot(111)
+        self.axes.axis('off')
+
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(self,
+                QSizePolicy.Expanding,
+                QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+        self.plotinput(sample_index)
+
+    def plotinput(self,dataindex):
+        ax1 = self.figure.add_subplot(111)
+        ax1.axis('off')
+        ax1.imshow(k[dataindex,:,:,0],cmap='gray')
+
+        #ax.set_title('PyQt Matplotlib Example')
+        self.draw()
+
+
+
 
 
 class PlotCanvas(FigureCanvas):
 
-    def __init__(self, parent=None, width=14, height=8,layer=None, dpi=100,sample_index=None):
+    def __init__(self, parent=None, width=9, height=9,layer=None, dpi=100,sample_index=None):
 
         fig = Figure(figsize=(width, height), dpi=dpi)
 
@@ -104,6 +134,7 @@ class PlotCanvas(FigureCanvas):
         #intermediate_layer_model = Model(input=model.input, output=model.get_layer('convolution2d_4').output)
 
         intermediate_output = activations(model,layer,k,dataindex)
+        #intermediate_output=intermediate_output.squeeze()
         vm=None
         if (len(intermediate_output.shape)==2):
             intermediate_output=intermediate_output.reshape(1,1,1,intermediate_output.shape[1])
@@ -113,12 +144,28 @@ class PlotCanvas(FigureCanvas):
         result=np.concatenate()
         '''
         nbofplots=intermediate_output.shape[3]
+        imagesize=intermediate_output.shape[1]
+        '''
         for i in range(nbofplots):
                     ax = self.figure.add_subplot(int(np.sqrt(nbofplots))+1,int(np.sqrt(nbofplots))+1,i+1)
                     ax.axis('off')
 
                     ax.imshow(intermediate_output[0,:,:,i],vmin=0,vmax=vm,cmap='gray')
+        '''
 
+        ncolumns=nraws=math.ceil(np.sqrt(nbofplots))
+
+        ishow=np.zeros([imagesize*ncolumns,imagesize*ncolumns])
+        intermediate_output=np.swapaxes(intermediate_output,0,3)
+        print([ncolumns,nbofplots])
+        for i in range(ncolumns):
+            print([i,imagesize*(nbofplots-i*ncolumns)])
+            ishow[i*imagesize:(i+1)*imagesize,0:imagesize*(nbofplots-i*ncolumns)]=np.hstack(intermediate_output[i*ncolumns:(i+1)*ncolumns,:,:,0])
+
+        #c=np.vstack(ishow)
+        ax = self.figure.add_subplot(111)
+        ax.axis('off')
+        ax.imshow(ishow[:,:],vmin=0,vmax=vm,cmap='gray')
 
         #ax.set_title('PyQt Matplotlib Example')
         self.draw()
