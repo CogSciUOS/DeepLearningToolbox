bead70bd1f697620a8e9bfd198005f04f8bbaba2
Rasmus Diederichsen <rdiederichse@uos.de>
2017-11-24 11:17:20 +0100

    add docstrings and remove dead code

--- qtgui/widgets/imageview.py
+++ qtgui/widgets/imageview.py
@@ -30,7 +30,7 @@ class QImageView(QWidget):
 
     def setImage(self, image: np.ndarray):
         if image is not None:
-            # To construct a 8-bit monochrome QImage, we need a
+            # To construct an 8-bit monochrome QImage, we need a
             # 2-dimensional, uint8 numpy array
             if image.ndim == 4:
                 image = image[0]
@@ -60,7 +60,7 @@ class QImageView(QWidget):
 
     def setActivationMask(self, mask, position=None):
         '''Set an (activation) mask to be displayed on top of
-        the actual image. The mask should be
+        the actual image.
 
         Parameters
         ----------
@@ -96,42 +96,31 @@ class QImageView(QWidget):
         painter.end()
 
     def _drawImage(self, painter: QPainter):
+        '''Draw current image into this ``QImageView``.
+
+        Parameters
+        ----------
+        painter :   QPainter
+        '''
         if self._image is not None:
+            __import__('ipdb').set_trace()
             w = self._image.width()
             h = self._image.height()
-            if False:
-                # Scale to full size
-                painter.drawImage(self.rect(), self._image)
-
-            elif True:
-                # scale maximally while maintaining aspect ratio
-                w_ratio = self.width() / w
-                h_ratio = self.height() / h
-                ratio = min(w_ratio, h_ratio)
-                rect = QRect((self.width() - w * ratio) // 2,
-                             (self.height() - h * ratio) // 2,
-                             w * ratio, h * ratio)
-                self._imageRect = rect
-                painter.drawImage(rect, self._image)
-
-            elif False:
-                # Original image size
-                x = (self.width() - self._image.width()) // 2
-                y = (self.height() - self._image.height()) // 2
-                painter.drawImage(x, y, self._image)
+            # scale maximally while maintaining aspect ratio
+            w_ratio = self.width() / w
+            h_ratio = self.height() / h
+            ratio = min(w_ratio, h_ratio)
+            self._imageRect = QRect((self.width() - w * ratio) // 2,
+                    (self.height() - h * ratio) // 2,
+                    w * ratio, h * ratio)
+            painter.drawImage(self._imageRect, self._image)
 
     def _drawMask(self, painter: QPainter):
-        '''Display the given image. Image is supposed to be a numpy array.
+        '''Display the given image.
+
+        Parameters
+        ----------
+        painter :   QPainter
         '''
         if self._image is not None and self._overlay is not None:
-            # __import__('ipdb').set_trace()
-            # scale_width = self.width() / self._image.width()
-            # scale_height = self.height() / self._image.height()
-            # delta = (self._image.size() - self._overlay.size())/2
-            # target = QRect(delta.width()*scale_width,
-            #                delta.height()*scale_height,
-            #                self._overlay.width()*scale_width,
-            #                self._overlay.height()*scale_height)
-
-            #source = QRect(QPoint(),self._overlay.size())
             painter.drawImage(self._imageRect, self._overlay)
