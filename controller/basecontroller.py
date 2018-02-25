eee0fcec2ab8b98c47dd802b604b9277c6e53083
Rasmus Diederichsen <rdiederichse@uos.de>
2018-02-25 13:07:16 +0100

    more doc fixes

--- controller/basecontroller.py
+++ controller/basecontroller.py
@@ -8,16 +8,17 @@ from PyQt5.QtCore import QObject, pyqtSignal
 class AsyncRunner(object):
     '''Base class for runner objects which must be provided for each controller/user interface.
 
-    Why is this necessary?
-    ----------------------
-    When working with Qt for instance, it is not possible to update the gui from a non-main thread.
-    So we cannot simply run a thread to do expensive computations and have it call
-    :py:meth:`model.Model.notifyObservers` since that call would invoke methods on
-    :py:class:`PyQt5.QWidget` s. We would need some way to call back into the main thread, but that
-    one is busy runnig Qt's main loop so the App can respond to further user action. Qt's way of
-    resolving this is with signals. But we do not want to make this a fixed solution since the app
-    should be as independent of Qt as possible. I have thus decided that a dedicated runner must be
-    provided for each kind of user interface which knows how to handle asynchronous updates.
+    .. note:: *Why is this necessary?*
+
+        When working with Qt for instance, it is not possible to update the gui from a non-main
+        thread.  So we cannot simply run a thread to do expensive computations and have it call
+        :py:meth:`model.Model.notifyObservers` since that call would invoke methods on
+        :py:class:`PyQt5.QWidget` s. We would need some way to call back into the main thread, but
+        that one is busy runnig Qt's main loop so the app can respond to further user action. Qt's
+        way of resolving this is with signals. But we do not want to make this a fixed solution
+        since the app should be as independent of Qt as possible. I have thus decided that a
+        dedicated runner must be provided for each kind of user interface which knows how to handle
+        asynchronous updates.
 
     Attributes
     ----------
@@ -153,6 +154,6 @@ class InputController(object):
         Parameters
         ----------
         mode    :   str
-                    One of 'dir' or 'array'
+                    One of ``dir`` or ``array``
         '''
         self._runner.runTask(self._model.setMode, mode)
