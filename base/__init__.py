40e2a1b78c9d0ed3bf89a9d20b6829bdd75bc736
Ulf Krumnack <krumnack@uos.de>
2019-01-18 17:02:58 +0100

    new base module

---
+++ base/__init__.py
@@ -0,0 +1,7 @@
+"""base: a module providing base functionality for the deep
+visualization toolbox.
+
+This module should not depend on other modules from the toolbox.
+"""
+
+from .config import Config
